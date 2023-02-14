0b56fa49140dd74b44728ae4897101bf03d34a80
Marek Stempkowski <mark.stempkowski@gmail.com>
2023-02-14 10:02:59 +0100

    OK test - testStartingValues

--- src/main/java/mareks/scoreboard/common/Match.java
+++ src/main/java/mareks/scoreboard/common/Match.java
@@ -1,11 +1,15 @@
 package mareks.scoreboard.common;
 
 public class Match {
+
+    int scoreHomeTeam = 0;
+    int scoreAwayTeam = 0;
+
     public int getScoreHomeTeam() {
-        return 1;
+        return scoreHomeTeam;
     }
 
     public int getScoreAwayTeam() {
-        return 1;
+        return scoreAwayTeam;
     }
 }
